e51b3f3d77851159bd76a52c6092b7e9c1a6924a
sidneysm <sidneysoaresm@gmail.com>
2015-11-14 00:32:50 -0300

    Adicionado visões de criar e adicionar Tarefas

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Tarefa
 
 # Create your views here.
 def tarefa_list(request):
-	return render(request, 'core/tarefa_list.html', {})
+	tarefas = Tarefa.objects.all().order_by('nome')
+	return render(request, 'core/tarefa_list.html', {'tarefas': tarefas})
